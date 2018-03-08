1ac9aada78e9e9252d53d4f5b2284c8606adc51e
starchainstc <starchainstc@gmail.com>
2018-03-08 19:33:52 +0800

    the code for contract

---
+++ core/contract/program/Program.go
@@ -0,0 +1,46 @@
+package program
+
+import (
+	"starchain/common/serialization"
+	. "starchain/errors"
+	"io"
+)
+
+type Program struct {
+
+	//the contract program code,which will be run on VM or specific envrionment
+	Code []byte
+
+	//the program code's parameter
+	Parameter []byte
+}
+
+//Serialize the Program
+func (p *Program) Serialize(w io.Writer) error {
+	if err := serialization.WriteVarBytes(w, p.Parameter); err != nil {
+		return NewDetailErr(err, ErrNoCode, "Execute Program Serialize Parameter failed.")
+	}
+
+	if err := serialization.WriteVarBytes(w, p.Code); err != nil {
+		return NewDetailErr(err, ErrNoCode, "Execute Program Serialize Code failed.")
+	}
+
+	return nil
+}
+
+//Deserialize the Program
+func (p *Program) Deserialize(w io.Reader) error {
+	parameter, err := serialization.ReadVarBytes(w)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "Execute Program Deserialize Parameter failed.")
+	}
+	p.Parameter = parameter
+
+	code, err := serialization.ReadVarBytes(w)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "Execute Program Deserialize Code failed.")
+	}
+	p.Code = code
+
+	return nil
+}
