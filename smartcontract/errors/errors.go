b6ba7b70df86576e280596766585aebc9de8ddbd
starchainstc <starchainstc@gmail.com>
2018-03-10 14:40:11 +0800

    the code for smartcontract

---
+++ smartcontract/errors/errors.go
@@ -0,0 +1,10 @@
+package errors
+
+import "errors"
+
+var (
+	ErrAssetNameInvalid          = errors.New("asset name invalid")
+	ErrAssetPrecisionInvalid     = errors.New("asset precision invalid")
+	ErrAssetAmountInvalid        = errors.New("asset amount invalid")
+)
+
