93b2678d8b94bb459e36bb6f83ab1a63079cb892
starchainstc <starchainstc@gmail.com>
2018-03-14 18:57:06 +0800

    the code of signature module

--- core/ledger/ledger.go
+++ core/ledger/ledger.go
@@ -67,3 +67,43 @@ func (l *Ledger) GetAsset(assetId Uint256) (*asset.Asset, error) {
 	}
 	return asset, nil
 }
+
+func (l *Ledger) GetBlockWithHeight(height uint32) (*Block, error) {
+	temp, err := l.Store.GetBlockHash(height)
+	if err != nil {
+		return nil, NewDetailErr(err, ErrNoCode, "[Ledger],GetBlockWithHeight failed with height="+string(height))
+	}
+	bk, err := DefaultLedger.Store.GetBlock(temp)
+	if err != nil {
+		return nil, NewDetailErr(err, ErrNoCode, "[Ledger],GetBlockWithHeight failed with hash="+temp.ToString())
+	}
+	return bk, nil
+}
+
+//Get block with block hash.
+func (l *Ledger) GetBlockWithHash(hash Uint256) (*Block, error) {
+	bk, err := l.Store.GetBlock(hash)
+	if err != nil {
+		return nil, NewDetailErr(err, ErrNoCode, "[Ledger],GetBlockWithHeight failed with hash="+hash.ToString())
+	}
+	return bk, nil
+}
+
+//BlockInLedger checks if the block existed in ledger
+func (l *Ledger) BlockInLedger(hash Uint256) bool {
+	return l.Store.IsBlockInStore(hash)
+}
+
+//Get transaction with hash.
+func (l *Ledger) GetTransactionWithHash(hash Uint256) (*tx.Transaction, error) {
+	tx, err := l.Store.GetTransaction(hash)
+	if err != nil {
+		return nil, NewDetailErr(err, ErrNoCode, "[Ledger],GetTransactionWithHash failed with hash="+hash.ToString())
+	}
+	return tx, nil
+}
+
+//Get local block chain height.
+func (l *Ledger) GetLocalBlockChainHeight() uint32 {
+	return l.Blockchain.BlockHeight
+}
