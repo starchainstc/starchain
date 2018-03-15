2f43341315d5c1618eadd12a88144cfdecee4ae0
starchainstc <starchainstc@gmail.com>
2018-03-15 18:30:04 +0800

    the code of events module

---
+++ events/event.go
@@ -0,0 +1,73 @@
+package events
+
+import (
+	"sync"
+	"errors"
+)
+
+type Event struct {
+	m           sync.RWMutex
+	subscribers map[EventType]map[Subscriber]EventFunc
+}
+
+
+func NewEvent() *Event {
+	return &Event{
+		subscribers: make(map[EventType]map[Subscriber]EventFunc),
+	}
+}
+
+
+func (e *Event) Subscribe(eventType EventType,eventFunc EventFunc) Subscriber{
+	e.m.Lock()
+	defer e.m.Unlock()
+	sub := make(chan interface{})
+	_,ok := e.subscribers[eventType]
+	if !ok {
+		e.subscribers[eventType] = make(map[Subscriber]EventFunc)
+	}
+	e.subscribers[eventType][sub] = eventFunc
+
+	return sub
+}
+
+func (e *Event) UnSubscribe(eventtype EventType,subscriber Subscriber) (err error){
+	e.m.Lock()
+	defer e.m.Unlock()
+
+	subEvent,ok := e.subscribers[eventtype]
+	if !ok {
+		err = errors.New("No event type.")
+		return
+	}
+
+	delete(subEvent,subscriber)
+	close(subscriber)
+
+	return
+}
+
+//Notify subscribers that Subscribe specified event
+func (e *Event) Notify(eventtype EventType,value interface{}) (err error){
+	e.m.RLock()
+	defer e.m.RUnlock()
+
+	subs,ok := e.subscribers[eventtype]
+	if !ok {
+		err = errors.New("No event type.")
+		return
+	}
+
+	for _, eventfunc := range subs {
+		go e.NotifySubscriber(eventfunc,value)
+	}
+	return
+}
+
+func (e *Event) NotifySubscriber(eventfunc EventFunc, value interface{}) {
+	if eventfunc == nil { return }
+
+	//invode subscriber event func
+	eventfunc(value)
+
+}
