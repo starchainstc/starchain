dc74fcbe268d9cbcf5885ea3337d315aa421c3b2
starchainstc <starchainstc@gmail.com>
2018-03-29 12:15:52 +0800

    modify some code for common module

---
+++ client.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/urfave/cli"
+	"starchain/client"
+	"starchain/client/asset"
+	"os"
+	"starchain/client/wallet"
+)
+
+func main(){
+	app := cli.NewApp()
+	app.Name = "stc-client"
+	app.Version = "v0.0.1"
+	app.HelpName = "stc-client"
+	app.Usage = "command line tool for STC blockchain"
+	app.UsageText = "stc-client [global options] command [command options] [args]"
+	app.HideHelp = false
+	app.HideVersion = false
+
+	app.Flags = []cli.Flag{
+		client.NewIpFlag(),
+		client.NewPortFlag(),
+	}
+	app.Commands = []cli.Command{
+		*asset.NewCommand(),
+		*wallet.NewCommand(),
+	}
+	app.Run(os.Args)
+}
